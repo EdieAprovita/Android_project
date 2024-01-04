b6bc7b2ca7924bed29caa998e769641a94351cbf
Edgar Chaver <edieveg316@gmail.com>
2024-01-04 17:42:07 -0600

    Pagination
    Adding pagination functionality

--- src/components/NewsList.tsx
+++ src/components/NewsList.tsx
@@ -1,10 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 import useNews from "../hooks/useNews";
 import NewsCard from "./NewsCard";
-import { Grid } from "@mui/material";
+import { Grid, Stack, Pagination } from "@mui/material";
 
 const NewsList: React.FC = () => {
-	const { news, loading, error } = useNews();
+	const [page, setPage] = useState<number>(1);
+	const { news, loading, error, totalPages } = useNews(page);
+
+	const handlePageChange = (event: React.ChangeEvent<unknown>, value: number) => {
+		setPage(value);
+	};
 
 	if (loading) {
 		return <div>Loading...</div>;
@@ -14,21 +19,37 @@ const NewsList: React.FC = () => {
 	}
 
 	return (
-		<Grid container spacing={5}>
-			{news.map(item => (
-				<Grid
-					item
-					xs={12}
-					sm={6}
-					md={4}
-					lg={3}
-					key={
-						item.source.id ? `${item.source.id}-${item.publishedAt}` : item.publishedAt
-					}>
-					<NewsCard newsItem={item} />
-				</Grid>
-			))}
-		</Grid>
+		<>
+			<Grid container spacing={5}>
+				{news.map(item => (
+					<Grid
+						item
+						xs={12}
+						sm={6}
+						md={4}
+						lg={3}
+						key={
+							item.source.id ? `${item.source.id}-${item.publishedAt}` : item.publishedAt
+						}>
+						<NewsCard newsItem={item} />
+					</Grid>
+				))}
+			</Grid>
+			<Stack spacing={2} sx={{ marginTop: 2 }}>
+				<Pagination
+					count={totalPages}
+					page={page}
+					onChange={handlePageChange}
+					color="primary"
+					shape="rounded"
+					sx={{
+						display: "flex",
+						justifyContent: "center",
+						marginY: 2,
+					}}
+				/>
+			</Stack>
+		</>
 	);
 };
 
--- src/hooks/useNews.tsx
+++ src/hooks/useNews.tsx
@@ -5,19 +5,29 @@ import { NewsItem } from "../models/News";
 const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
 const BASE_URL = "https://newsapi.org/v2";
 
-const useNews = () => {
+const useNews = (page: number) => {
 	const [news, setNews] = useState<NewsItem[]>([]);
 	const [loading, setLoading] = useState<boolean>(true);
 	const [error, setError] = useState<string | null>(null);
+	const [totalPages, setTotalPages] = useState<number>(0);
+	const pageSize = 10;
 
 	useEffect(() => {
 		const fetchNews = async () => {
 			try {
+				setLoading(true);
 				const response = await axios.get(
-					`${BASE_URL}/everything?q=bitcoin&apiKey=${API_KEY}`
+					`${BASE_URL}/everything?q=bitcoin&apiKey=${API_KEY}`,
+					{
+						params: {
+							page,
+							pageSize,
+						},
+					}
 				);
 				setNews(response.data.articles);
-				console.log(response.data.articles);
+				const totalResults = response.data.totalResults;
+				setTotalPages(Math.ceil(totalResults / pageSize));
 			} catch (error) {
 				setError((error as Error).message);
 			} finally {
@@ -25,9 +35,9 @@ const useNews = () => {
 			}
 		};
 		fetchNews();
-	}, []);
+	}, [page]);
 
-	return { news, loading, error };
+	return { news, loading, error, totalPages };
 };
 
 export default useNews;
